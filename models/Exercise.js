a587d7d38613f35a85010287741e95569faf5b55
James Strande <strandejr@gmail.com>
2021-11-06 12:20:23 -0400

    db modeling

---
+++ models/Exercise.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    name: String,
+    type: String,
+    weight: Number,
+    sets: Number,
+    reps: Number,
+    duration: Number,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+module.exports = Exercise;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    Exercise: require('./Exercise'),
+    Stats: require('./Stats'),
+    Workout: require('./Workout')
+}
