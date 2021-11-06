9d222b9a4750f8e497018452b20a38a877db68a9
James Strande <strandejr@gmail.com>
2021-11-06 12:02:37 -0400

    server connected to database

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const { application } = require("express");
 
 
+const PORT = process.env.PORT || 3000;
+
+const db = require('./models');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/workoutsdb", 
@@ -12,3 +25,6 @@ mongoose.connect(
         useFindAndModify: false 
     }
 );
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`)
+});
